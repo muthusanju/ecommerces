03cc158dd25fec4c3f6c9cfd3a9491a72db20233
muthusanju <67627300+muthusanju@users.noreply.github.com>
2020-07-22 19:31:17 +0530

    I have add image.js file

---
+++ module/Image.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const ImageSchema = mongoose.Schema({
+  image: {  type: String,
+    required: true
+}
+});
+
+// export model user with UserSchema
+module.exports = mongoose.model("images", ImageSchema);
